591193d9fb3662899e2f79f0ca1e491f16f025e7
ph1109ji <ph1109ji@gmail.com>
2022-02-23 14:51:44 +0700

    Adding font, basic setup for layer base and components , empty assets folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline font-jakarta">Hello world!</h1>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Display", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
