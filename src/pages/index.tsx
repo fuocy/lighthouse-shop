6fe16a704f1ec1c9ba6791b04ec669bd6553b542
ph1109ji <ph1109ji@gmail.com>
2022-02-23 14:29:46 +0700

    Setup: NextJS TypeScript TailwindCSS Folder-Structure Absolute-Import Custom-Documents

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default Home;
+
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   // ...
+// }
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // ...
+// }
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // ...
+// }
